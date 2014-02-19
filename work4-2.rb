7b87ea310bcd0069d4a67a8d62f630aa0aecd370
olive <aya0061@gmail.com>
2014-02-19 09:44:26 +0900

    ページングできた！

--- work4-2.rb
+++ work4-2.rb
@@ -11,40 +11,45 @@ get '/work/4-2/?page=:id' do
   pagecount = params[:id]
 
   # テンプレートで使う変数には「@」をつける
-  @restaurants = db.execute('SELECT * FROM restaurants LIMIT 5 OFFSET ?', [params[:id]*5])
+  @restaurants = db.execute('SELECT * FROM restaurants LIMIT 5 OFFSET ?', [params[:id].to_i * 5])
 
   # prevtest = params[:id].to_i - 1
   # データの件数を取得
+  @countall = db.execute('SELECT count(*) FROM restaurants')[0]['count(*)']
 
-  @countall = db.execute('SELECT count(*) FROM restaurants')
-  @countall2 = "SELECT count(*) FROM restaurants"
+  #ページ数
+  @pagecountall = @countall / 5
 
   #【"前へ" ページが０の時非表示にする】◯以上のとき以外は非表示
   unless params[:id].to_i <= 0
-   @prev = "<a href=""/work/4-2/page=#{params[:id].to_i - 1 }"">まえへ</a>"
+   @prev = "<a href='/work/4-2/page=#{params[:id].to_i - 1 }'><<前へ</a>"
   end
 
   #【"次へ" ページが最後の時非表示にする】◯以上のときは非表示
-  # unless params[:id].to_i >= @countall.to_i
-  #  @prev = "<a href=""/work/4-2/page=#{params[:id].to_i - 1 }"">まえへ</a>"
-  # end
+  unless params[:id].to_i <= 0 || params[:id].to_i >= @pagecountall
+  @next = "<a href='/work/4-2/page=#{params[:id].to_i + 1 }'>>>次へ</a>"
+  end
+
+
+  # 前後 のページを数字でリンクを出す
+
+  unless params[:id].to_i <= 0 || params[:id].to_i-1 >= @pagecountall
+    @prev1 = "<li><a href='/work/4-2/page=#{ params[:id].to_i - 1 }'>#{params[:id].to_i - 1}</a></li>"
+  end
+  unless params[:id].to_i-2 <= 0 || params[:id].to_i-2 >= @pagecountall
+    @prev2 = "<li><a href='/work/4-2/page=#{ params[:id].to_i - 2 }'>#{params[:id].to_i - 2}</a></li>"
+  end
+
+  unless params[:id].to_i+1 <= 0 || params[:id].to_i >= @pagecountall
+    @next1 = "<li><a href='/work/4-2/page=#{ params[:id].to_i + 1 }'>#{params[:id].to_i + 1}</a></li>"
+  end
+  unless params[:id].to_i+2 <= 0 || params[:id].to_i+ 1 >= @pagecountall
+   @next2 = "<li><a href='/work/4-2/page=#{ params[:id].to_i + 2 }'>#{params[:id].to_i + 2}</a></li>"
+  end
 
 
 
-  @next = "/work/4-2/page=#{ params[:id].to_i + 1 }"
-
-  @next1 = "/work/4-2/page=#{ params[:id].to_i + 1 }"
-  @next2 = "/work/4-2/page=#{ params[:id].to_i + 2 }"
-  @next3 = "/work/4-2/page=#{ params[:id].to_i + 3 }"
-  @next4 = "/work/4-2/page=#{ params[:id].to_i + 4 }"
-
-  @prev1 = "/work/4-2/page=#{ params[:id].to_i - 1 }"
-  @prev2 = "/work/4-2/page=#{ params[:id].to_i - 2 }"
-  @prev3 = "/work/4-2/page=#{ params[:id].to_i - 3 }"
-  @prev4 = "/work/4-2/page=#{ params[:id].to_i - 4 }"
-
-
-  # `views/test4_4_index.erb`テンプレートとして使う
+  # テンプレートとして使う
   erb :work4_2_index
 end
 
